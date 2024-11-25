e9fc154b8a6e27a3d571f2ffc4162e5c6edb3ee9
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2024-11-25 09:30:51 +0200

    solved problems

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,20 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+    args.forEach((arg, index) => {
+        const name = types[index].name.toLowerCase();
+        if (typeof arg !== name) {
+            throw new TypeError(`Argument type ${name} expected`);
+          }
+    });
+
+  const res = fn(...args);
+  const def = types[types.length - 1];
+  const name = def.name.toLowerCase();
+  if (typeof res !== name) {
+    throw new TypeError(`Result type ${name} expected`);
+  }
+  return res;
+};
 
 module.exports = { contract };
